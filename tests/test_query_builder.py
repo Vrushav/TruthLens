31d5475cc5f9a2a95bd3dc6c7866f5a4b806c9e2
Vrushav Patel <vrushavpatel1021@gmail.com>
2026-07-19 09:54:58 +0530

    feat: implement intelligent query builder

---
+++ tests/test_query_builder.py
@@ -0,0 +1,17 @@
+from src.models.claim import Claim
+from src.models.entity import Entity
+from src.modules.query_builder import QueryBuilder
+
+
+builder = QueryBuilder()
+
+claim = Claim(
+    text="Python was created by Guido van Rossum."
+)
+
+claim.entities = [
+    Entity("Python", "TECHNOLOGY"),
+    Entity("Guido van Rossum", "PERSON")
+]
+
+print(builder.build(claim))
